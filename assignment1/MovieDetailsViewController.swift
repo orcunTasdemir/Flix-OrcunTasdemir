07aeddc3b8fcbb9fd67c94226b3f59651d01be4e
orcunTasdemir <otasdemir@vassar.edu>
2021-12-03 12:45:19 -0500

    almost working with the trailerview
    almost working with the trailerview

--- assignment1/MovieDetailsViewController.swift
+++ assignment1/MovieDetailsViewController.swift
@@ -11,6 +11,12 @@ import AlamofireImage
 class MovieDetailsViewController: UIViewController {
     
     
+    @IBAction func posterTapAction(_ sender: UITapGestureRecognizer) {
+        print("poster tapped!")
+    }
+    
+    
+    
     @IBOutlet weak var backdropView: UIImageView!
     @IBOutlet weak var posterView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
@@ -19,6 +25,8 @@ class MovieDetailsViewController: UIViewController {
     
     //look up swift optionals
     var movie: [String:Any]!
+    var getVideos = [[String: Any]]()
+    var trailerUrl: URL!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,14 +55,47 @@ class MovieDetailsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+    
+    //     MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //     In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            //Get the new view controller using segue.destination.
+            //Pass the selected object to the new view controller.
+            print("Loading up the trailers screen!")
+            
+            let movieId = movie["id"] as! Int
+            let movieIdS = String(movieId)
+            let urlBegin = "https://api.themoviedb.org/3/movie/"
+            let urlEnd = "/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US"
+            let url = URL(string: urlBegin + movieIdS + urlEnd)!
+            print("url: ", url)
+            
+            let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+            let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+            let task = session.dataTask(with: request) { [self] (data, response, error) in
+                 // This will run when the network request returns
+                 if let error = error {
+                        print(error.localizedDescription)
+                 } else if let data = data {
+                        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                     
+                     self.getVideos = dataDictionary["results"] as! [[String:Any]]
+                     print(getVideos)
+                     let getVideo = getVideos[0]
+                     print("get video: ", getVideo)
+                     let key = getVideo["key"] as! String
+                     print("key: ", key)
+                     let youtubeUrl = "https://www.youtube.com/watch?v="
+         
+                     trailerUrl = URL(string: youtubeUrl + key)
+                     print(trailerUrl)
+         
+                     let trailerViewController = segue.destination as! TrailerViewController
+                     trailerViewController.trailerUrl = trailerUrl
+                     print("passing it here: ", trailerUrl)
+                 }
+            }
+            task.resume()
+        }
     }
-    */
-
-}
---
+++ assignment1/TrailerViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TrailerViewController.swift
+//  assignment1
+//
+//  Created by OrcunTasdemir on 12/2/21.
+//
+
+import UIKit
+import WebKit
+
+class TrailerViewController: UIViewController, WKUIDelegate {
+    
+    
+    @IBOutlet weak var trailerView: WKWebView!
+    
+    //look up swift optionals
+    var trailerUrl: URL!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("passed version: ", trailerUrl)
+        // Do any additional setup after loading the view.
+        let myURL = URL(string:"https://www.apple.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+    }
+    
+    var webView: WKWebView!
+        
+        override func loadView() {
+            let webConfiguration = WKWebViewConfiguration()
+            webView = WKWebView(frame: .zero, configuration: webConfiguration)
+            webView.uiDelegate = self
+            view = webView
+        }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
